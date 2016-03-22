15a28dd8e6afcd966574867f26c4b13b4b676bd2
SamuelColacchia <samuel.colacchia@gordon.edu>
2016-03-22 12:29:32 -0400

    Implimented Paren()

--- project3.cc
+++ project3.cc
@@ -9,36 +9,35 @@ using namespace std;
  *
  */
 
-typedef ThreadedBinarySearchTree::Iterator Iterator;
+typedef ThreadedBinarySearchTree::Iterator   Iterator;
 
 ThreadedBinarySearchTree::ThreadedBinarySearchTree()
-        : _header(new  Node())
+  : _header(new  Node())
 {
-        _header->_lchild = makeThread(_header);
-        _header->_rchild = _header;
+  _header->_lchild = makeThread(_header);
+  _header->_rchild = _header;
 }
 
 
 ThreadedBinarySearchTree::~ThreadedBinarySearchTree()
 {
+  //Adam and Dr Tuck
+  Iterator it;
+  Node *tempnode;
 
-        //Adam and Dr Tuck
-        Iterator it;
-        Node * tempnode;
-        for (it = this->inorder_begin(); it != this->end(); it++)
-        {
-                //temp node = to pointer
+  for (it = this->inorder_begin(); it != this->end(); it++)
+  {
+    //temp node = to pointer
 
-                tempnode = it._ptr;
+    tempnode = it._ptr;
 
-                delete it._ptr;
+    delete it._ptr;
+  }
+  // delete pointer;
 
-        }
-        // delete pointer;
-
-        delete _header;
-
+  delete _header;
 }
+
 
 /*
  * Insert function called when the user calls i in the program
@@ -47,25 +46,21 @@ ThreadedBinarySearchTree::~ThreadedBinarySearchTree()
  */
 void ThreadedBinarySearchTree::insert(string key, int value)
 {
+  //Deal with the case that are first item is the header
+  if (empty())
+  {
+    Node *newnode = new Node();
+    _header->_lchild = newnode;
 
-
-        //Deal with the case that are first item is the header
-        if (empty())
-        {
-                Node *newnode = new Node();
-                _header->_lchild = newnode;
-
-                newnode->_lchild = makeThread(_header);
-                newnode->_rchild = makeThread(_header);
-                newnode->_value = value;
-                newnode->_key = key;
-
-
-        }
-        else
-        {
-                ThreadedBinarySearchTree::insertr(key, value, _header->_lchild);
-        }
+    newnode->_lchild = makeThread(_header);
+    newnode->_rchild = makeThread(_header);
+    newnode->_value = value;
+    newnode->_key = key;
+  }
+  else
+  {
+    ThreadedBinarySearchTree::insertr(key, value, _header->_lchild);
+  }
 }
 
 
@@ -77,43 +72,42 @@ void ThreadedBinarySearchTree::insert(string key, int value)
  */
 void ThreadedBinarySearchTree::insertr(string key, int value, Node *& root)
 {
+  if (isThread(root->_lchild) && (key < root->_key))
+  {
+    Node *newnode = new Node();
 
-        if (isThread(root->_lchild) && key < root->_key)
-        {
-                Node *newnode = new Node();
+    newnode->_lchild = root->_lchild;
+    newnode->_rchild = makeThread(root);
 
-                newnode->_lchild = root->_lchild;
-                newnode->_rchild = makeThread(root);
+    root->_lchild = newnode;
 
-                root->_lchild = newnode;
+    newnode->_value = value;
+    newnode->_key = key;
+  }
+  else if (isThread(root->_rchild) && (key > root->_key))
+  {
+    Node *newnode = new Node();
 
-                newnode->_value = value;
-                newnode->_key = key;
-        }
-        else if (isThread(root->_rchild) && key > root->_key)
-        {
-                Node *newnode = new Node();
+    newnode->_lchild = makeThread(root);
+    newnode->_rchild = root->_rchild;
 
-                newnode->_lchild = makeThread(root);
-                newnode->_rchild = root->_rchild;
+    root->_rchild = newnode;
 
-                root->_rchild = newnode;
+    newnode->_value = value;
+    newnode->_key = key;
+  }
+  else
+  {
+    if (key > root->_key)
+    {
+      insertr(key, value, root->_rchild);
+    }
 
-                newnode->_value = value;
-                newnode->_key = key;
-        }
-        else
-        {
-                if (key > root->_key)
-                {
-                        insertr(key, value, root->_rchild);
-                }
-
-                else if (key < root->_key)
-                {
-                        insertr(key, value, root->_lchild);
-                }
-        }
+    else if (key < root->_key)
+    {
+      insertr(key, value, root->_lchild);
+    }
+  }
 }
 
 
@@ -125,83 +119,79 @@ void ThreadedBinarySearchTree::erase(Iterator iter)
 //NOTE | TESTED and works as intended
 bool ThreadedBinarySearchTree::empty() const
 {
-        if (makePointer(_header->_lchild) == _header)
-        {
-                return true;
-        }
-        else
-        {
-                return false;
-        }
+  if (makePointer(_header->_lchild) == _header)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 
 Iterator ThreadedBinarySearchTree::lookup(string key) const
 {
-
-        if (empty())
-        {
-                return end();
-        }
-        else
-        {
-                Iterator it;
-                for (it = this->inorder_begin(); it != this->end(); it++)
-                {
-                        if (key == it._ptr->_key) {
-                                return it;
-                        }
-                }
-                return end();
-        }
+  if (empty())
+  {
+    return end();
+  }
+  else
+  {
+    Iterator it;
+    for (it = this->inorder_begin(); it != this->end(); it++)
+    {
+      if (key == it._ptr->_key)
+      {
+        return it;
+      }
+    }
+    return end();
+  }
 
 //        ThreadedBinarySearchTree::lookupr(key, _header);
-
 }
-
-
-
 
 
 Iterator ThreadedBinarySearchTree::inorder_begin() const
 {
-        return Iterator(_header, _header, Iterator::INORDER).insucc();
+  return Iterator(_header, _header, Iterator::INORDER).insucc();
 }
 
 
 Iterator ThreadedBinarySearchTree::inorder_rbegin() const
 {
-        return Iterator(_header, _header, Iterator::INORDER).inpred();
+  return Iterator(_header, _header, Iterator::INORDER).inpred();
 }
 
 
 Iterator ThreadedBinarySearchTree::preorder_begin() const
 {
-        return Iterator(_header, _header, Iterator::PREORDER).presucc();
+  return Iterator(_header, _header, Iterator::PREORDER).presucc();
 }
 
 
 Iterator ThreadedBinarySearchTree::preorder_rbegin() const
 {
-        return Iterator(_header, _header, Iterator::PREORDER).prepred();
+  return Iterator(_header, _header, Iterator::PREORDER).prepred();
 }
 
 
 Iterator ThreadedBinarySearchTree::postorder_begin() const
 {
-        return Iterator(_header, _header, Iterator::POSTORDER).postsucc();
+  return Iterator(_header, _header, Iterator::POSTORDER).postsucc();
 }
 
 
 Iterator ThreadedBinarySearchTree::postorder_rbegin() const
 {
-        return Iterator(_header, _header, Iterator::POSTORDER).postpred();
+  return Iterator(_header, _header, Iterator::POSTORDER).postpred();
 }
 
 
 Iterator ThreadedBinarySearchTree::end() const
 {
-        return Iterator(_header, _header, Iterator::UNDEFINED);
+  return Iterator(_header, _header, Iterator::UNDEFINED);
 }
 
 
@@ -214,19 +204,19 @@ Iterator ThreadedBinarySearchTree::end() const
 
 bool ThreadedBinarySearchTree::isThread(Node *ptr)
 {
-        return (((long)ptr) & THREAD_FLAG_MASK) != 0;
+  return (((long)ptr) & THREAD_FLAG_MASK) != 0;
 }
 
 
 ThreadedBinarySearchTree::Node *ThreadedBinarySearchTree::makeThread(Node *ptr)
 {
-        return (Node *)(((long)ptr) | THREAD_FLAG_MASK);
+  return (Node *)(((long)ptr) | THREAD_FLAG_MASK);
 }
 
 
 ThreadedBinarySearchTree::Node *ThreadedBinarySearchTree::makePointer(Node *thread)
 {
-        return (Node *)(((long)thread) & ~THREAD_FLAG_MASK);
+  return (Node *)(((long)thread) & ~THREAD_FLAG_MASK);
 }
 
 
@@ -236,174 +226,209 @@ ThreadedBinarySearchTree::Node *ThreadedBinarySearchTree::makePointer(Node *thre
  */
 
 Iterator::Iterator()
-        : _header(NULL), _ptr(NULL), _order(UNDEFINED)
+  : _header(NULL), _ptr(NULL), _order(UNDEFINED)
 {
 }
 
 
 Iterator& Iterator::operator ++()
 {
-        switch (_order)
-        {
-        case INORDER:
-                return insucc();
+  switch (_order)
+  {
+  case INORDER:
+    return insucc();
 
-        case PREORDER:
-                return presucc();
+  case PREORDER:
+    return presucc();
 
-        case POSTORDER:
-                return postsucc();
+  case POSTORDER:
+    return postsucc();
 
-        default:
-                return *this;
-        }
+  default:
+    return *this;
+  }
 }
 
 
 Iterator& Iterator::operator --()
 {
-        switch (_order)
-        {
-        case INORDER:
-                return inpred();
+  switch (_order)
+  {
+  case INORDER:
+    return inpred();
 
-        case PREORDER:
-                return prepred();
+  case PREORDER:
+    return prepred();
 
-        case POSTORDER:
-                return postpred();
+  case POSTORDER:
+    return postpred();
 
-        default:
-                return *this;
-        }
+  default:
+    return *this;
+  }
 }
 
 
 Iterator Iterator::operator ++(int)
 {
-        Iterator result = *this;
+  Iterator result = *this;
 
-        operator ++();
-        return result;
+  operator ++();
+  return result;
 }
 
 
 Iterator Iterator::operator --(int)
 {
-        Iterator result = *this;
+  Iterator result = *this;
 
-        operator --();
-        return result;
+  operator --();
+  return result;
 }
 
 
 //NOTE | Untested
 Iterator& Iterator::insucc()
 {
-        if (isThread(_ptr->_rchild))
-        {
-                _ptr = makePointer(_ptr->_rchild);
-        }
-        else
-        {
-                _ptr = _ptr->_rchild;
-                while (!isThread(_ptr->_lchild))
-                {
-                        _ptr = _ptr->_lchild;
-                }
-        }
-        return *this;
+  if (isThread(_ptr->_rchild))
+  {
+    _ptr = makePointer(_ptr->_rchild);
+  }
+  else
+  {
+    _ptr = _ptr->_rchild;
+    while (!isThread(_ptr->_lchild))
+    {
+      _ptr = _ptr->_lchild;
+    }
+  }
+  return *this;
 }
 
 
 Iterator& Iterator::inpred()
 {
-        if (isThread(_ptr->_lchild))
-        {
-                _ptr = makePointer(_ptr->_lchild);
-        }
-        else
-        {
-                _ptr = _ptr->_lchild;
-                while (!isThread(_ptr->_rchild))
-                {
-                        _ptr = _ptr->_rchild;
-                }
-        }
-        return *this;
+  if (isThread(_ptr->_lchild))
+  {
+    _ptr = makePointer(_ptr->_lchild);
+  }
+  else
+  {
+    _ptr = _ptr->_lchild;
+    while (!isThread(_ptr->_rchild))
+    {
+      _ptr = _ptr->_rchild;
+    }
+  }
+  return *this;
 }
+
 
 //close but getting a core dump error in the else statment
 Iterator& Iterator::presucc()
 {
-        if (!isThread(_ptr->_lchild))
-                _ptr = _ptr->_lchild;
+  if (!isThread(_ptr->_lchild))
+  {
+    _ptr = _ptr->_lchild;
+  }
 
-        else if (!isThread(_ptr->_rchild))
-                _ptr = _ptr->_rchild;
-        else{
-          while(isThread(_ptr->_rchild))
-          {
-            _ptr = makePointer(_ptr->_rchild);
-          }
-          _ptr = _ptr->_rchild;
-        }
-         return *this;
+  else if (!isThread(_ptr->_rchild))
+  {
+    _ptr = _ptr->_rchild;
+  }
+  else
+  {
+    while (isThread(_ptr->_rchild))
+    {
+      _ptr = makePointer(_ptr->_rchild);
+    }
+    _ptr = _ptr->_rchild;
+  }
+  return *this;
 }
 
 
 Iterator& Iterator::prepred()
 { /* STUB - REPLACE WITH REAL CODE */
-        return *this;
+  return *this;
 }
 
 
 Iterator& Iterator::postsucc()
 { /* STUB - REPLACE WITH REAL CODE */
-        return *this;
+  return *this;
 }
 
 
 Iterator& Iterator::postpred()
 { /* STUB - REPLACE WITH REAL CODE */
-        return *this;
+  return *this;
 }
 
 
 Iterator Iterator::parent() const
-{ /* STUB - REPLACE WITH REAL CODE */
-        return Iterator(_header, (Node *)_header, Iterator::UNDEFINED);
+{
+  bool found = false;
+  Node *tempnode = (Node*)_header;
+
+  while (!found)
+  {
+    if (!isThread(tempnode->_lchild) && key() == tempnode->_lchild->_key)
+    {
+      return Iterator(_header, tempnode, Iterator::UNDEFINED);
+    }
+    else if (!isThread(tempnode->_rchild) && key() == tempnode->_rchild->_key)
+    {
+      return Iterator(_header, tempnode, Iterator::UNDEFINED);
+    }
+    else
+    {
+      if (tempnode == _header)
+      {
+        tempnode = tempnode->_lchild;
+      }
+      else if (key() > tempnode->_key)
+      {
+        tempnode = tempnode->_rchild;
+      }
+
+      else if (key() < tempnode->_key)
+      {
+        tempnode = tempnode->_lchild;
+      }
+    }
+  }
 }
 
 
 string Iterator::key() const
 {
-        return _ptr->_key;
+  return _ptr->_key;
 }
 
 
 int Iterator::value() const
 {
-        return _ptr->_value;
+  return _ptr->_value;
 }
 
 
 bool Iterator::operator ==(const Iterator& other) const
 {
-        return _ptr == other._ptr && _ptr != NULL;
+  return _ptr == other._ptr && _ptr != NULL;
 }
 
 
 bool Iterator::operator !=(const Iterator& other) const
 {
-        return _ptr != other._ptr || _ptr == NULL;
+  return _ptr != other._ptr || _ptr == NULL;
 }
 
 
 Iterator::Iterator(const Node *header,
                    Node       *ptr,
-                   Order order)
-        : _header(header), _ptr(ptr), _order(order)
+                   Order      order)
+  : _header(header), _ptr(ptr), _order(order)
 {
 }
 
